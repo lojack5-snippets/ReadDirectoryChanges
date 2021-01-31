226902261c2809df11887fcc78507d2ec986e57f
lojack5 <1458329+lojack5@users.noreply.github.com>
2021-01-30 21:48:54 -0700

    split example out to its own file

---
+++ example.py
@@ -0,0 +1,34 @@
+from pathlib import Path
+from functools import partial
+
+import trio
+
+from read_changes import Listener, Watcher
+
+class PrintListener(Listener):
+    async def on_added(self, path):
+        print('File Created:', path)
+
+    async def on_modified(self, path):
+        print('File Modified:', path)
+
+    async def on_removed(self, path):
+        print('File Removed:', path)
+
+    async def on_renamed(self, old_path, new_path):
+        print('File Renamed:', old_path, '->', new_path)
+
+    async def on_scan_needed(self):
+        print('Scan needed!')
+
+
+async def main(directory):
+    async with trio.open_nursery() as nursery:
+        listener = PrintListener()
+        watcher = Watcher()
+        nursery.start_soon(partial(listener.listen, watcher.create_event_reciever()))
+        nursery.start_soon(partial(watcher.watch, directory))
+
+
+if __name__ == '__main__':
+    trio.run(partial(main, Path.cwd()))
--- read_changes.py
+++ read_changes.py
@@ -1,5 +1,3 @@
-from functools import partial
-from pathlib import Path
 import math
 
 import trio
@@ -251,32 +249,3 @@ class Listener:
     async def on_removed(self, path): pass
     async def on_renamed(self, old_path, new_path): pass
     async def on_scan_needed(self): pass
-
-
-class PrintListener(Listener):
-    async def on_added(self, path):
-        print('File Created:', path)
-
-    async def on_modified(self, path):
-        print('File Modified:', path)
-
-    async def on_removed(self, path):
-        print('File Removed:', path)
-
-    async def on_renamed(self, old_path, new_path):
-        print('File Renamed:', old_path, '->', new_path)
-
-    async def on_scan_needed(self):
-        print('Scan needed!')
-
-
-async def main(directory):
-    async with trio.open_nursery() as nursery:
-        listener = PrintListener()
-        watcher = Watcher()
-        nursery.start_soon(partial(listener.listen, watcher.create_event_reciever()))
-        nursery.start_soon(partial(watcher.watch, directory))
-
-
-if __name__ == '__main__':
-    trio.run(partial(main, Path.cwd()))
